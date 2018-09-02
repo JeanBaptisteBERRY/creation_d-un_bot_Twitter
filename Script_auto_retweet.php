90641e56954f2c4b44c31499ca09334be9f16c27
Jean-Baptiste BERRY <jean_baptiste.berry@utt.fr>
2018-09-02 15:43:13 +0200

    Update Script_auto_retweet.php

--- Script_auto_retweet.php
+++ Script_auto_retweet.php
@@ -1,1 +1,28 @@
+<?php
 
+session_start();
+require_once("C:/Users/BigData/Downloads/twitteroauth-master/twitteroauth-master/twitteroauth/twitteroauth.php"); //Path to twitteroauth library
+$consumer_key = "";//YOUR CONSUMER KEY
+$consumer_secret = "";//YOUR CONSUMER KEY SECRET
+$access_key = "";//YOUR TOKEN ACCESS
+$access_secret = "";//YOUR TOKEN ACCESS SECRET
+function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+$connection = getConnectionWithAccessToken($consumer_key, $consumer_secret, $access_key, $access_secret);
+$theSearch = [
+ 'q' => '#Kamikaze',//CHOOSE HERE YOUR HASHTAGS
+ 'count' => 780//NUMBER OF TWEETS
+  // 'location' => 'Paris', YOU CAN ADD THE LOCATION HERE
+];
+ 
+// 'location' => 'Paris',
+$results = $connection->get('search/tweets', $theSearch);
+ 
+foreach($results->statuses as $status) {
+$connection->post('statuses/retweet', [
+ 'id' => $status->id_str
+]);
+}
+?>
